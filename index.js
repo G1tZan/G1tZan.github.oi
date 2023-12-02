f100acacd8c8f72046bdf3201f04cb29910de18b
G1tZan <149072236+G1tZan@users.noreply.github.com>
2023-12-02 21:18:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,75 +1,79 @@
 var character = document.getElementById("character");
-        var result = document.getElementById("result");
-        var game = document.getElementById("game");
-        var score = document.getElementById("score");
-        var finalScore = document.getElementById("finalScore");
-        var count = 0;
-        var sound = document.getElementById("sound");
-        var isJumping = false;
-    
-        function gameOver() {
-            document.getElementById("gameOverSound").play(); 
-        }
-    
-        function jump() {
-            if (!isJumping) {
-                isJumping = true;
-                sound.play();
-                var jumpCount = 0;
-                var jumpInterval = setInterval(function() {
+var result = document.getElementById("result");
+var game = document.getElementById("game");
+var score = document.getElementById("score");
+var finalScore = document.getElementById("finalScore");
+var count = 0;
+var sound = document.getElementById("sound");
+var isJumping = false;
+var gameStarted = false; // Add a variable to track if the game has started
+
+function gameOver() {
+    document.getElementById("gameOverSound").play();
+}
+
+function jump() {
+    if (gameStarted && !isJumping) { // Check if the game has started before allowing the jump
+        isJumping = true;
+        sound.play();
+        var jumpCount = 0;
+        var jumpInterval = setInterval(function () {
+            var characterBottom = parseInt(window.getComputedStyle(character).getPropertyValue("bottom"));
+            if ((characterBottom < 170) && (jumpCount < 20)) {
+                character.style.bottom = (characterBottom + 10) + "px";
+            } else if (jumpCount >= 15) {
+                var fallInterval = setInterval(function () {
                     var characterBottom = parseInt(window.getComputedStyle(character).getPropertyValue("bottom"));
-                    if ((characterBottom < 170) && (jumpCount < 20)) {
-                        character.style.bottom = (characterBottom + 10) + "px";
-                    } else if (jumpCount >= 15) {
-                        var fallInterval = setInterval(function() {
-                            var characterBottom = parseInt(window.getComputedStyle(character).getPropertyValue("bottom"));
-                            if ((characterBottom > 0)) {
-                                character.style.bottom = (characterBottom - 10) + "px";
-                            } else {
-                                clearInterval(fallInterval);
-                                isJumping = false;
-                            }
-                        }, 15);
-                        clearInterval(jumpInterval);
+                    if ((characterBottom > 0)) {
+                        character.style.bottom = (characterBottom - 10) + "px";
+                    } else {
+                        clearInterval(fallInterval);
+                        isJumping = false;
                     }
-                    jumpCount++;
-                }, 20);
+                }, 15);
+                clearInterval(jumpInterval);
             }
+            jumpCount++;
+        }, 20);
+    }
+}
+
+function startGame() {
+    gameStarted = true; // Set the game as started when starting the game
+    document.getElementById("startScreen").style.display = "none";
+    document.getElementById("game").style.display = "block";
+}
+
+function restartGame() {
+    location.reload();
+}
+
+document.addEventListener("keydown", function (event) {
+    if (event.code === "Space") {
+        event.preventDefault();
+        startGame();
+        jump();
+    }
+});
+
+var cacti = document.querySelectorAll(".cactus");
+
+setInterval(function checkCollision() {
+    cacti.forEach(function (cactus) {
+        var cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+        var characterBottom = parseInt(window.getComputedStyle(character).getPropertyValue("bottom"));
+        if ((cactusLeft < 60) && (characterBottom < 80)) {
+            result.style.display = "block";
+            game.style.display = "none";
+            finalScore.innerHTML = `Score: ${count}`;
+            gameOver();
         }
-    
-        function startGame() {
-            document.getElementById("startScreen").style.display = "none";
-            document.getElementById("game").style.display = "block";
-        }
-    
-        function restartGame() {
-            location.reload();
-        }
-    
-        document.addEventListener("keydown", function(event) {
-            if (event.code === "Space") {
-                event.preventDefault();
-                startGame();
-                jump();
-            }
-        });
-    
-        var cacti = document.querySelectorAll(".cactus");
-    
-        setInterval(function checkCollision() {
-            cacti.forEach(function(cactus) {
-                var cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
-                var characterBottom = parseInt(window.getComputedStyle(character).getPropertyValue("bottom"));
-                if ((cactusLeft < 60) && (characterBottom < 80)) {
-                    result.style.display = "block"; 
-                    game.style.display = "none";
-                    finalScore.innerHTML = `Score: ${count}`;
-                    gameOver();
-                }
-            });
-        }, 10);
-    
-        setInterval(function updateScore() {
-            count++;
-            score.innerHTML = `Score: ${count}`;
-        }, 1000);
+    });
+}, 10);
+
+setInterval(function updateScore() {
+    if (gameStarted) { // Update score only if the game has started
+        count++;
+        score.innerHTML = `Score: ${count}`;
+    }
+}, 1000);
